cf652a045074403e7fa16b582f72066fbdf87310
Aaron Jones <jon17049@byui.edu>
2022-05-19 20:44:46 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

---
+++ pong/point_p.py
@@ -0,0 +1,22 @@
+"""
+ Program:
+    CS241 Assignment 05, Re-Create the Pong Game
+ Instructor:
+    Borther Macbeth
+ Author:
+    Aaron Jones
+ Summary: 
+    This Class will set the inital state values for
+    both x and y axis as well as set each point to
+    a floating point value. 
+                     
+"""
+
+class Point:
+    """
+    * Sets the inital state values
+    * for the Point class.
+    """
+    def __init__(self):
+        self.x = 0.0
+        self.y = 0.0
