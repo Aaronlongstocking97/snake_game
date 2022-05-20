5c83fb4681dcd74ddf54f0b37524d2bbc502ebb7
Aaron Jones <jon17049@byui.edu>
2022-05-19 20:23:16 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- game.py
+++ game.py
@@ -19,12 +19,12 @@
 
 from global_ import *
 import arcade
-# from bullet import Bullet
-# from asteroid import Asteroid
+from bullet import Bullet
+from asteroid import Asteroid
 from ship import Ship
-# from large_asteroid import LargeAsteroid
-# from medium_asteroid import MediumAsteroid
-# from small_asteroid import SmallAsteroid
+from large_asteroid import LargeAsteroid
+from medium_asteroid import MediumAsteroid
+from small_asteroid import SmallAsteroid
 
 """
 * This class handles all the game callbacks and interaction
@@ -42,10 +42,10 @@ class Game(arcade.Window):
         super().__init__(width, height)
         arcade.set_background_color(arcade.color.SMOKY_BLACK)
         self.ship = Ship()
-        # self.asteroids = [LargeAsteroid(), LargeAsteroid(), LargeAsteroid(),
-        #                   LargeAsteroid(), LargeAsteroid()]
+        self.asteroids = [LargeAsteroid(), LargeAsteroid(), LargeAsteroid(),
+                          LargeAsteroid(), LargeAsteroid()]
         self.held_keys = set()
-        # self.bullets = []
+        self.bullets = []
 
     """
     * Called automatically by the arcade framework.
@@ -56,11 +56,11 @@ class Game(arcade.Window):
 
         arcade.start_render()
 
-        # for bullet in self.bullets:
-        #     bullet.draw()
+        for bullet in self.bullets:
+            bullet.draw()
 
-        # for asteroid in self.asteroids:
-        #     asteroid.draw()
+        for asteroid in self.asteroids:
+            asteroid.draw()
 
         if self.ship.alive == True:
             self.ship.draw()
@@ -73,14 +73,14 @@ class Game(arcade.Window):
     def update(self, delta_time):
 
         self.check_keys()
-        # self.check_collisions()
+        self.check_collisions()
         self.ship.advance()
 
-        # for bullet in self.bullets:
-        #     bullet.advance()
+        for bullet in self.bullets:
+            bullet.advance()
 
-        # for asteroid in self.asteroids:
-        #     asteroid.advance()
+        for asteroid in self.asteroids:
+            asteroid.advance()
 
     """
     * Checks to see if bullets have hit targets.
@@ -88,41 +88,41 @@ class Game(arcade.Window):
     * :return:
     """
 
-    # def check_collisions(self):
+    def check_collisions(self):
 
-    #     new_asteroid = []
-    #     for bullet in self.bullets:
-    #         for asteroid in self.asteroids:
-    #             if bullet.alive and asteroid.alive:
-    #                 too_close = bullet.radius + asteroid.radius
-    #                 if (abs(bullet.center.x - asteroid.center.x) < too_close and
-    #                         abs(bullet.center.y - asteroid.center.y) < too_close):
-    #                     bullet.alive = False
-    #                     new_asteroid += asteroid.hit()
-    #         self.asteroids += new_asteroid
-    #     for asteroid in self.asteroids:
-    #         if asteroid.alive and self.ship.alive:
-    #             too_close = asteroid.radius + self.ship.radius
-    #             if (abs(asteroid.center.x - self.ship.center.x) < too_close and
-    #                     abs(asteroid.center.y - self.ship.center.y) < too_close):
-    #                 self.ship.alive = False
+        new_asteroid = []
+        for bullet in self.bullets:
+            for asteroid in self.asteroids:
+                if bullet.alive and asteroid.alive:
+                    too_close = bullet.radius + asteroid.radius
+                    if (abs(bullet.center.x - asteroid.center.x) < too_close and
+                            abs(bullet.center.y - asteroid.center.y) < too_close):
+                        bullet.alive = False
+                        new_asteroid += asteroid.hit()
+            self.asteroids += new_asteroid
+        for asteroid in self.asteroids:
+            if asteroid.alive and self.ship.alive:
+                too_close = asteroid.radius + self.ship.radius
+                if (abs(asteroid.center.x - self.ship.center.x) < too_close and
+                        abs(asteroid.center.y - self.ship.center.y) < too_close):
+                    self.ship.alive = False
 
-    #     self.cleanup_zombies()
+        self.cleanup_zombies()
 
     """
     * Removes any dead bullets or asteroids from the list.
     * :return:
     """
 
-    # def cleanup_zombies(self):
+    def cleanup_zombies(self):
 
-    #     for bullet in self.bullets:
-    #         if not bullet.alive:
-    #             self.bullets.remove(bullet)
+        for bullet in self.bullets:
+            if not bullet.alive:
+                self.bullets.remove(bullet)
 
-    #     for asteroid in self.asteroids:
-    #         if not asteroid.alive:
-    #             self.asteroids.remove(asteroid)
+        for asteroid in self.asteroids:
+            if not asteroid.alive:
+                self.asteroids.remove(asteroid)
 
     """
     * This function checks for keys that are being held down.
@@ -154,11 +154,11 @@ class Game(arcade.Window):
         if self.ship.alive:
             self.held_keys.add(key)
 
-            # if key == arcade.key.SPACE:
-            #     bullet = Bullet(self.ship)
-            #     bullet.fire()
-            #     bullet.advance()
-            #     self.bullets.append(bullet)
+            if key == arcade.key.SPACE:
+                bullet = Bullet(self.ship)
+                bullet.fire()
+                bullet.advance()
+                self.bullets.append(bullet)
 
     """
     * Removes the current key from the set of held keys.
