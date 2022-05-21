fef5934158d737707320d377a8a741773c0b6a93
Aaron Jones <jon17049@byui.edu>
2022-05-20 20:05:54 -0600

    Signed-off-by: Aaron Jones <jon17049@byui.edu>

--- pong/pong_game_sandbox.py
+++ pong/pong_game_sandbox.py
@@ -14,7 +14,7 @@
 """
 import arcade  # Gives the class the ability to make objects
 
-from global_pong import *  # Takes in values from the global constants
+from global_ import *  # Takes in values from the global constants
 from fruit import Fruit
 from snake import Snake
 
